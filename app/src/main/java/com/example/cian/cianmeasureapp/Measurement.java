83e2532846043f805aafb8d980a16466774455b3
Cian <cian.mchale.2016@mumail.ie>
2019-03-26 17:40:28 +0000

    Updated

---
+++ app/src/main/java/com/example/cian/cianmeasureapp/Measurement.java
@@ -0,0 +1,64 @@
+package com.example.cian.cianmeasureapp;
+
+import android.speech.SpeechRecognizer;
+
+public class Measurement {
+
+    private String pic;
+    private String location;
+    private String distance;
+    private String description;
+    private String review;
+    private String rating;
+
+    public Measurement() {
+    }
+
+    public String getPic() {
+        return pic;
+    }
+
+    public void setPic(String pic) {
+        this.pic = pic;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public String getDistance() {
+        return distance;
+    }
+
+    public void setDistance(String distance) {
+        this.distance = distance;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getReview() {
+        return review;
+    }
+
+    public void setReview(String review) {
+        this.review = review;
+    }
+
+    public String getRating() {
+        return rating;
+    }
+
+    public void setRating(String rating) {
+        this.rating = rating;
+    }
+}
